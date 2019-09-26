062e7798f7603b49c3eceefa127d2b66a8e04e4d
Nick <nick.moi@gmail.com>
2019-09-26 03:29:24 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,9 +5,5 @@ def run_guessing_game
   user_input = gets.chomp
   if user_input == randNum
     puts "You guessed the correct number!"
-  elsif randNum != user_input
-    puts "Sorry! The computer guessed #{randNum}"
-  elsif user_input == "exit"
-    puts "Goodbye!"
   end
 end
